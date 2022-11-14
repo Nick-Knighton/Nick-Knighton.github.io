e38cdeb5f95183cfda145c54fa6ba5cb1bf1841e
Nick Knighton <nickk@tamu.edu>
2022-11-14 15:04:22 -0600

    added more style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,248 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Card} from 'react-bootstrap';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div >
+      <div class="wave"></div>
+      <div class="gridLayout">
+
+      <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Campus Resources
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://studentlife.tamu.edu/lgbtq/resources/">LGBTQ+ campus resources</a></li>
+              <li><a href="https://diversity.tamu.edu/Resources">Respurces for diversity</a></li>
+              <li><a href="https://allies.tamu.edu/resources/">Aggie ally</a></li>
+              <li><a href="https://studentlife.tamu.edu/wrc/about/">Womens resource center</a></li>
+              <li><a href="https://studentlife.tamu.edu/lgbtq/">LGBTQ+ pride center</a></li>
+              <li><a href="https://caps.tamu.edu/diversity-inclusion/lgbtq/tamu-resources/">Counseling and psychological services</a></li>
+              <li><a href="https://studentlife.tamu.edu/hp/topics/sex/">TAMU Health and wellness center for STDs</a></li>
+              </ul>
+          </div>
+        </Card>
+        
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Normal Body Images
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.007b.com/breast_gallery.php">Normal breast gallery</a></li>
+              <li><a href="https://labialibrary.org.au/">Labia library</a></li>
+              <li><a href="https://www.willyworries.com/the-picture-galleries/the-willy-gallery/">What a normal penis looks like</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Birth Control
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.texaswearscondoms.com/">Free condoms (texaswearscondoms)</a></li>
+              <li><a href="https://www.cdc.gov/condomeffectiveness/docs/male-condom-use-508.pdf">How to put on a male condom</a></li>
+              <li><a href="https://www.cdc.gov/condomeffectiveness/internal-condom-use.html#:~:text=Find%20a%20comfortable%20position.,until%20it%20rests%20against%20cervix.">How to put on a female condom</a></li>
+              <li><a href="https://www.cdc.gov/condomeffectiveness/Dental-dam-use.html">How to use a dental dam</a></li>
+              <li><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3649591/">The history of the male condom</a></li>
+              <li><a href="https://www.pbs.org/wgbh/americanexperience/features/pill-birth-control-pill/">Birth control before the pill</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Anatomy
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://vimeo.com/222111805">A history of the clitoris</a></li>
+              <li><a href="https://www.youtube.com/watch?v=4qbUdVk-QvE">The penis</a></li>
+              <li><a href="https://www.youtube.com/watch?v=kgb1wdHqsvk">Vulvas & vaginas</a></li>
+              <li><a href="https://www.britannica.com/science/human-body">Anatomy of the human body</a></li>
+              <li><a href="https://www.ashasexualhealth.org/sexual-and-reproductive-anatomy/#:~:text=Sexual%20anatomy%20typically%20refers%20to,doesn't%20determine%20their%20gender.">Sexual anatomy</a></li>
+              <li><a href="https://kidshealth.org/en/teens/female-repro.html#:~:text=A%20female's%20internal%20reproductive%20organs,vagina%20can%20expand%20and%20contract.">Female reproductive anatomy</a></li>
+              <li><a href="https://my.clevelandclinic.org/health/articles/9117-male-reproductive-system">Male reproductive anatomy</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Periods
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.unicef.org/stories/how-girls-boys-are-busting-period-myths-5-countries">Busting period myths around the world</a></li>
+              <li><a href="https://www.womenshealth.gov/menstrual-cycle/your-menstrual-cycle#:~:text=Menstruation%20is%20a%20woman's%20monthly,your%20body%20through%20your%20vagina.">Periods explained</a></li>
+              <li><a href="https://www.chestercountyhospital.org/news/health-eliving-blog/2019/july/period-myths">Period Myths</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Orgasms
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://podcasts.apple.com/us/podcast/what-is-an-orgasm/id1480401609?i=1000450114409">What is an orgasm</a></li>
+              <li><a href="https://podcasts.apple.com/us/podcast/facilitating-and-inhibiting-orgasms/id1480401609?i=1000450114294">Facilitating and inhibiting orgasms</a></li>
+              <li><a href="https://podcasts.apple.com/us/podcast/the-sexual-response-cycle/id1480401609?i=1000450114283">The sexual response cycle</a></li>
+              <li><a href="https://www.youtube.com/watch?v=IarBYMXb5cg">How to have a clitoral orgasm</a></li>
+              <li><a href="https://www.youtube.com/watch?v=pXVDOPyg_hU">Vaginal orgasms</a></li>
+              <li><a href="https://www.youtube.com/watch?v=IarBYMXb5cg&t=5s">Orgasmic sex positions</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Gender
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://diversity.tamu.edu/Resources">Campus gender resources</a></li>
+              <li><a href="https://www.glaad.org/transgender/transfaq">Transgender FAQs</a></li>
+              <li><a href="https://www.youtube.com/watch?v=lrYx7HaUlMY">Ted Talk: I've Lived As a Man and a Woman</a></li>
+              <li><a href="https://www.youtube.com/watch?v=yAHCqnux2fk">Raising a transgender child</a></li>
+              <li><a href="https://www.npr.org/2021/06/02/996319297/gender-identity-pronouns-expression-guide-lgbtq">A Guide to understanding gender identity terms</a></li>
+              <li><a href="https://www.youtube.com/watch?v=IarBYMXb5cg&t=5s">Orgasmic sex positions</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Sexual Orientation
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://studentlife.tamu.edu/lgbtq/about/">LGBTQ pride center</a></li>
+              <li><a href="https://www.apa.org/topics/lgbtq/orientation">Answer to questions about sexual orientation</a></li>
+              <li><a href="https://www.plannedparenthood.org/learn/sexual-orientation/sexual-orientation">Sexual orientation vs gender</a></li>
+              <li><a href="https://www.apa.org/topics/lgbtq/sexual-orientation">Sexual orientation and gender identity</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Sexual Orientation
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://studentlife.tamu.edu/lgbtq/about/">LGBTQ pride center</a></li>
+              <li><a href="https://www.apa.org/topics/lgbtq/orientation">Answer to questions about sexual orientation</a></li>
+              <li><a href="https://www.plannedparenthood.org/learn/sexual-orientation/sexual-orientation">Sexual orientation vs gender</a></li>
+              <li><a href="https://www.apa.org/topics/lgbtq/sexual-orientation">Sexual orientation and gender identity</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Sex
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.youtube.com/watch?v=0TGOSu9YuFc">Foreplay</a></li>
+              <li><a href="https://www.youtube.com/watch?v=IarBYMXb5cg">Orgasmic sex positions</a></li>
+              <li><a href="https://www.youtube.com/user/sexplanations">Sexplanations youtube channel</a></li>
+              <li><a href="https://www.netflix.com/title/81160763#:~:text=From%20the%20biology%20of%20attraction,this%20entertaining%20and%20enlightening%20series.&text=Watch%20all%20you%20want.,at%20the%20facts%20of%20life.">Netflix "Sex Explained" show</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Love and Relationships
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://studentlife.tamu.edu/hp/topics/ivp/dvpam/">TAMU domestic violence preventation and awareness month</a></li>
+              <li><a href="https://www.thehotline.org/?utm_source=google&utm_medium=organic&utm_campaign=domestic_violence">Domestic violence website</a></li>
+              <li><a href="https://www.psychologytoday.com/us/blog/intimate-portrait/201704/speaking-your-partners-love-language-may-not-matter">Speaking your partners love language may not matter</a></li>
+              <li><a href="https://www.gottman.com/about/research/">Gottman love research</a></li>
+              <li><a href="https://www.npr.org/2013/10/04/228906644/pioneering-masters-of-sex-brought-science-to-the-bedroom">Masters and Johnson love research</a></li>
+              <li><a href="https://www.mindbodygreen.com/articles/the-5-love-languages-explained">The 5 love languages</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Sexual Concerns
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.huffpost.com/entry/what-is-sex-therapy_n_58c1b4efe4b0d1078ca5378c">Sexual therapy</a></li>
+              <li><a href="https://www.psychologytoday.com/us/blog/all-about-sex/201908/does-pornography-cause-erectile-dysfunction">Does pornography cause erectile dysfunction?</a></li>
+              <li><a href="https://5miweekly.com/sex-iq-question-14-can-erectile-dysfunction-be-successfully-treated/">Can erectile dysfunction be successfully treated?</a></li>
+              <li><a href="https://www.singlecare.com/blog/how-does-viagra-work/#:~:text=The%20best%20way%20to%20know,cholesterol%2C%20or%20high%20blood%20pressure.">Is viagra right for me</a></li>
+              <li><a href="https://www.webmd.com/women/features/guide-to-female-viagra">Female viagra</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Paraphilias and Sexual Variants
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1115095/#:~:text=Sexual%20variations%20are%20also%20referred,revolving%20round%20particular%20sex%20objects.&text=They%20are%20much%20more%20common%20in%20men%20than%20women.">Sexual variations</a></li>
+              <li><a href="https://www.theatlantic.com/health/archive/2015/01/bdsm-versus-the-dsm/384138/">BDSM vs DSM</a></li>
+              <li><a href="https://www.medicinenet.com/paraphilia/article.htm">Paraphalias</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+          Sexually transmitted diseases
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.cdc.gov/std/general/default.htm">CDC STDs information</a></li>
+              <li><a href="https://studentlife.tamu.edu/hp/topics/sex/">TAMU Health and wellness center for STDs</a></li>
+              <li><a href="https://website-2830150933848082947126-stdtestingservice.business.site/?utm_source=gmb&utm_medium=referral">College Station STD testing</a></li>
+            </ul>
+          </div>
+        </Card>
+
+        <Card class="cardLayout">
+          <Card.Title style={{textAlign: 'center', margin: '1vh'}}>
+            Pornography
+          </Card.Title>
+          <div class='cardBody'>
+            <ul>
+              <li><a href="https://www.healthline.com/health/pornography-addiction#:~:text=Support%20groups&text=DailyStrength.org%3A%20Sex%2FPornography,1%2D800%2D662%2D4357">Pornography addiction treatment</a></li>
+              <li><a href="https://www.reddit.com/r/NoFap/">No fap subreddit</a></li>
+              <li><a href="https://www.addictioncenter.com/community/signs-of-porn-addiction/">10 signs of pornography addiction</a></li>
+              <li><a href="https://rewardfoundation.org/pornography-affects-health/#:~:text=Consuming%20internet%20pornography%20can%20change,Put%20simply%2C%20pornography%20affects%20health.">Is porn healthy?</a></li>
+            </ul>
+          </div>
+        </Card>
+        
+        
+      </div>
     </div>
+    
+      
+
+
+    
+      
+      
+    
+      
   );
 }
 
